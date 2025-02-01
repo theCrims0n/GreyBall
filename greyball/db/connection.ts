ae3bd9d72bf1ab1da5d3200a946cee7337c25351
theCrims0n <mi.salomon89@gmail.com>
2025-01-31 22:24:53 -0700

    c

--- greyball/db/connection.ts
+++ greyball/db/connection.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 const connection = async () => {
-    const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/store'
+    const MONGO_URL = process.env.MONGO_URL_CLOUD!
     await mongoose.connect(MONGO_URL).then(() => {
         console.log('DB is running correctly')
     }).catch((err) => {
